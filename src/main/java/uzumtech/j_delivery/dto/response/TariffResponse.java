a8031aeeef7324053dfc9c2210576b437c07cc55
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-23 02:09:01 +0500

    update

--- src/main/java/uzumtech/j_delivery/dto/response/TariffResponse.java
+++ src/main/java/uzumtech/j_delivery/dto/response/TariffResponse.java
@@ -1,6 +1,15 @@
 package uzumtech.j_delivery.dto.response;
 
+import lombok.Builder;
+
+@Builder
 public record TariffResponse(
-        Long id
+        Long id,
+        String name,
+        Double maxWeight,
+        Double maxLength,
+        Double maxWidth,
+        Double maxHeight,
+        String description
 ) {
 }
