08cf7e9f8131a490dc6035173dca7d591111555c
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-23 01:40:01 +0500

    create TariffRepository record

---
+++ src/main/java/uzumtech/j_delivery/repository/TariffRepository.java
@@ -0,0 +1,7 @@
+package uzumtech.j_delivery.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uzumtech.j_delivery.entity.TariffEntity;
+
+public interface TariffRepository extends JpaRepository<TariffEntity, Integer> {
+}
