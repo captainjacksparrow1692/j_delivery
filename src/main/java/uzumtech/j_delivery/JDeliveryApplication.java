6bee5282238fb247fd4a1e9fc8015e1440090e0a
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-23 01:10:23 +0500

    init

---
+++ src/main/java/uzumtech/j_delivery/JDeliveryApplication.java
@@ -0,0 +1,13 @@
+package uzumtech.j_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uzumtech/j_delivery/JDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package uzumtech.j_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
