7c1edaf25ec8b2e1128d5d68177fbc510051153e
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-24 01:43:53 +0500

    update

--- src/main/java/uzumtech/j_delivery/dto/response/OrderResponse.java
+++ src/main/java/uzumtech/j_delivery/dto/response/OrderResponse.java
@@ -2,10 +2,14 @@ package uzumtech.j_delivery.dto.response;
 
 import uzumtech.j_delivery.constant.enums.Status;
 
+import java.time.LocalDateTime;
+
 public record OrderResponse(
         Long id,
         Long tariffId,
         Double totalPrice,
-        Status status
+        Status status,
+        String trackingNumber,
+        LocalDateTime createdAt
 ) {
 }
