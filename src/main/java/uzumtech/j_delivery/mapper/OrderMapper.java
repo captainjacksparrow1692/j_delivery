0ba091ab39a12a849b563b7470bc38299bf5a593
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-25 21:33:12 +0500

    update

--- src/main/java/uzumtech/j_delivery/mapper/OrderMapper.java
+++ src/main/java/uzumtech/j_delivery/mapper/OrderMapper.java
@@ -2,18 +2,25 @@ package uzumtech.j_delivery.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import uzumtech.j_delivery.dto.request.OrderRequest;
 import uzumtech.j_delivery.dto.response.OrderResponse;
 import uzumtech.j_delivery.entity.OrderEntity;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
+
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "isActive", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "status", constant = "NEW")
+    @Mapping(target = "trackingNumber", ignore = true)
+    @Mapping(target = "totalPrice", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "distanceKm", source = "distance")
     OrderEntity toEntity(OrderRequest orderRequestDto);
 
-    OrderResponse toDto(OrderEntity orderEntity);
+    OrderResponse toResponse(OrderEntity orderEntity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "trackingNumber", ignore = true)
+    @Mapping(target = "distanceKm", source = "distance")
+    void updateEntityFromDto(OrderRequest dto, @MappingTarget OrderEntity entity);
 }
