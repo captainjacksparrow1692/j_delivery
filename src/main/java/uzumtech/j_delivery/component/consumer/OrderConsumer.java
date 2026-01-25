4c2c6c984273793b47d926d20a799420b463601d
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-25 21:49:24 +0500

    create OrderConsumer

---
+++ src/main/java/uzumtech/j_delivery/component/consumer/OrderConsumer.java
@@ -0,0 +1,24 @@
+package uzumtech.j_delivery.component.producer;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import uzumtech.j_delivery.dto.OrderDto;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class OrderConsumer {
+    @KafkaListener(topics = "${kafka.topic.order-topic}", containerFactory = "orderFactory")
+    public void orderListener(@Payload OrderDto payload, Acknowledgment acknowledgment) {
+        acknowledgment.acknowledge();
+
+        log.info("orderListener consumer {}", payload);
+    }
+}
