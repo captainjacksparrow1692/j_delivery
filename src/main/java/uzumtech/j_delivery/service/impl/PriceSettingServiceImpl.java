42d412cddd9123926f32d4b984290147a1e0ae6f
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-01-25 20:07:39 +0500

    create PriceSettingServiceImpl record

---
+++ src/main/java/uzumtech/j_delivery/service/impl/PriceSettingServiceImpl.java
@@ -0,0 +1,50 @@
+package uzumtech.j_delivery.service.impl;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+import uzumtech.j_delivery.dto.request.PriceSettingRequest;
+import uzumtech.j_delivery.dto.response.PriceSettingResponse;
+import uzumtech.j_delivery.entity.PriceSetting;
+import uzumtech.j_delivery.mapper.PriceSettingMapper;
+import uzumtech.j_delivery.repository.PriceSettingRepository;
+import uzumtech.j_delivery.service.PriceSettingService;
+
+public class PriceSettingServiceImpl implements PriceSettingService {
+    PriceSettingRepository repository;
+    PriceSettingMapper mapper;
+
+    @Override
+    @Transactional
+    public PriceSettingResponse create (PriceSettingRequest request) {
+        PriceSetting entity = mapper.toEntity(request);
+        PriceSetting saved = repository.save(entity);
+        return mapper.toResponse(saved);
+    }
+
+    @Override
+    public PriceSettingResponse get(Long id) {
+        return repository.findById(id)
+                .map(mapper::toResponse)
+                .orElseThrow(() -> new RuntimeException("Settings not found with id: " + id));
+    }
+
+    @Override
+    public PriceSettingResponse getCurrent() {
+        return repository.findFirstByOrderByUpdatedAtDesc()
+                .map(mapper::toResponse)
+                .orElseThrow(() -> new RuntimeException("Settings not found"));
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Page<PriceSettingResponse> getHistory(int page, int size) {
+        return repository.findAll(PageRequest.of(page, size))
+                .map(mapper::toResponse);
+    }
+}
